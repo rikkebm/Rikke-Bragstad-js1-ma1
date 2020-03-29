112d56b9ad6f91009e6e2bb1cd1623a03869e59b
rikkebm <60385097+rikkebm@users.noreply.github.com>
2020-03-29 15:36:41 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,62 @@
+// question 1
+//var cat = {
+    //complain: function() {
+        //console.log("Meow!");
+//}
+    //}
+    //cat.complain();
+
+
+// question 2
+//const heading = document.querySelector("h3");
+//console.dir(heading);
+
+
+// question 3
+//const heading = document.querySelector("h3");
+//heading.style.fontSize = "2em";
+
+
+// question 4
+//const heading = document.querySelector("h3");
+//const div = document.querySelector("div");
+//div.classList.add("subheading");
+//console.log(div.className);
+
+
+// question 5
+//const paragraphs = document.querySelectorAll("p");
+
+
+// question 6
+//const resultsContainer = document.querySelector(".results");
+//resultsContainer.innerHTML = `<p>New paragraph</p>;
+
+
+// question 7
+//function catFunction(catArray)
+//{
+    
+//}
+//const cats = [
+//{
+//name: "blob",
+//age: 10
+//},
+//{
+//name: "Harold"
+//},
+//{
+//name: "Blurt",
+//age: 21
+//}
+//];
+//}
+    //for (var i=0; i <catArray.length; i++){
+    //console.log(catArray[i].name);
+
+// question 8
+
+// question 9
+
+// question 10
